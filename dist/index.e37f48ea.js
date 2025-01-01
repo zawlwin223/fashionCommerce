1b1850de3379b38274371631aac979c8fd9b231d
zawlwin223 <zawlwinp223@gmail.com>
2025-01-01 12:59:07 -0800

    add_to_cart function

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -809,6 +809,7 @@ parcelHelpers.export(exports, "renderItem", ()=>renderItem);
 const detail_page = document.querySelector('.detail_page');
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
+let amount = 0;
 const getItem = function(control) {
     if (!id) return;
     control(id);
@@ -823,15 +824,19 @@ const renderItem = function(data) {
         <span>${data.category}</span>
         <h2>${data.title}</h2>
         <p class="price">$${data.price}</p>
-        <select value="Select Size" name="" id="">
+        <select value="Select Size" name="" class="size">
           <option value="">M</option>
           <option value="">S</option>
           <option value="">L</option>
           <option value="">XL</option>
         </select>
         <div>
-          <input type="number" />
-          <button>Add To Cart</button>
+          <div class="amount-container">
+    <button class="amount-button amount-button-decrease" onclick="decreaseAmount()">-</button>
+    <input type="text" id="amount" class="amount-value" value="0" readonly>
+    <button class="amount-button amount-button-increase" onclick="increaseAmount()">+</button>
+  </div>
+          <button class="add_to_cart">Add To Cart</button>
         </div>
         <h3>Product Description</h3>
         <p>
@@ -839,6 +844,40 @@ const renderItem = function(data) {
         </p>
       </article>`;
     detail_page.innerHTML = html;
+    //increase and decrease amount work
+    increaseAmount();
+    decreaseAmount();
+    addToCart(data);
+};
+function increaseAmount() {
+    const btn = document.querySelector('.amount-button-increase');
+    btn.addEventListener('click', ()=>{
+        amount++;
+        document.getElementById('amount').value = amount;
+    });
+}
+function decreaseAmount() {
+    const btn = document.querySelector('.amount-button-decrease');
+    btn.addEventListener('click', ()=>{
+        if (amount > 0) // Prevents negative values
+        amount--;
+        document.getElementById('amount').value = amount;
+    });
+}
+const addToCart = function(data) {
+    //get data size and amount
+    // const size = document.querySelector('.size').value
+    // const amount = document.getElementById('amount').value
+    // console.log(size, amount)
+    // console.log(data)
+    document.body.addEventListener('click', (e)=>{
+        if (e.target.classList.contains('add_to_cart')) {
+            const cart = JSON.parse(localStorage.getItem('cart')) || [];
+            cart.push(data);
+            localStorage.setItem('cart', JSON.stringify(data));
+            alert('Item added to cart');
+        }
+    });
 };
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["ik2hV","aenu9"], "aenu9", "parcelRequire94c2")
